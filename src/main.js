9b828a9d101a8221617ae283c2b7fa820cdc3673
Adrien Richard <you@example.com>
2025-03-13 13:51:38 +0100

    full screen map

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log('All good');
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+var lat = 45.166672;
+var lon = 5.71667;
+var macarte = null;
+// Fonction d'initialisation de la carte
+function initMap() {
+    // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
+    macarte = L.map('map').setView([lat, lon], 11);
+    // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attributionControl: false,
+        minZoom: 13,
+        maxZoom: 20
+    }).addTo(macarte);
+}
+window.onload = function () {
+    // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+    initMap();
+};
